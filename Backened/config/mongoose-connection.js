b354b54474fd221279f2f67b6616aadceffd8311
Sohel Darwajkar <sohaildarwajkarrocks@gmailc.om>
2025-01-26 17:55:06 +0530

    NewChange

--- Backened/config/mongoose-connection.js
+++ Backened/config/mongoose-connection.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 console.log("Environment Variables:", {
   NODE_ENV: process.env.NODE_ENV,
-  MONGO_URI_DEV: process.env.MONGO_URI_DEV,
-  MONGO_URI_PROD: process.env.MONGO_URI_PROD,
+  MONGO_URI_DEV: process.env.MONGODB_URI_DEV,
+  MONGO_URI_PROD: process.env.MONGODB_URI_PROD,
 });
 
 const dbURI =
   process.env.NODE_ENV === "production"
-    ? process.env.MONGO_URI_PROD
-    : process.env.MONGO_URI_DEV;
+    ? process.env.MONGODB_URI_PROD
+    : process.env.MONGODB_URI_DEV;
 console.log("Connecting to database with URI:", dbURI); // Check the URI value
 
 mongoose

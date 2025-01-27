6a6943bb7347e59c29e8f8f44e39ec69bb867bd5
Sohel Darwajkar <sohaildarwajkarrocks@gmailc.om>
2025-01-27 15:31:59 +0530

    c

--- Frontened/src/places/components/PlaceList.jsx
+++ Frontened/src/places/components/PlaceList.jsx
@@ -11,7 +11,7 @@ const PlaceList = (props) => {
   const auth = useContext(authContext);
   const location = useLocation();
   const isAllPlacesPage = location.pathname === "/places/all";
-  console.log("props recieved At PlaceList : ", JSON.stringify(props));
+  console.log("props recieved At PlaceList : ", props);
   if (!props.places || props.places.length === 0) {
     return (
       <div className="place-list center">
